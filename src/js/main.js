6b42aa862b96b5194fb00b1ba8d342d44e94920a
RENATA SCHMIDT <renalisa@live.unc.edu>
2021-10-28 21:19:32 -0400

    style, js, tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
+// Get reference what you need
+var button = document.querySelector("button");
+var ul = document.querySelector("ul");
 
+// Define what should happen
+function toggle() {
+  ul.classList.toggle("active");
+}
+
+// Wire up the event
+button.addEventListener("click", toggle);
